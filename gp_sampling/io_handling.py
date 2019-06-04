9f4c548fb13aa93a09ad9c33f0d97eb36a8b4180
Stefan <stefan.muehlbauer@uni-weimar.de>
2019-06-04 14:12:50 +0200

    completed wrapper for loading files

--- gp_sampling/io_handling.py
+++ gp_sampling/io_handling.py
@@ -1,3 +1,17 @@
-class A:
+import numpy as np
+import pandas as pd
+
+class FileLoader:
+    
     def __init__(self):
         pass
+    
+    @classmethod
+    def load_model_series(cls, path) -> np.ndarray:
+        unpckld = np.load(path)
+        return unpckld['mean'], unpckld['stds']
+    
+    @classmethod
+    def load_ground_truth(cls, path) -> pd.DataFrame:
+        gt = pd.DataFrame.from_csv(path)
+        return gt
