68db593636d9ac8baf88346672f6e9526b23eb7c
Stefan Mühlbauer <stefan.muehlbauer@uni-weimar>
2019-06-19 16:39:46 +0200

    upodate

--- gp_sampling/learning/learners.py
+++ gp_sampling/learning/learners.py
@@ -8,6 +8,23 @@ import numpy as np
 import pandas as pd
 import sklearn.preprocessing as preprocessing
 
+import tensorflow as tf
+
+
+class Brownian(gpflow.kernels.Kernel):
+
+    def __init__(self):
+        super().__init__(input_dim=1, active_dims=[0])
+        self.variance = gpflow.Param(1.0, transform=gpflow.transforms.positive)
+
+    def K(self, X, X2=None):
+        if X2 is None:
+            X2 = X
+        return self.variance * tf.minimum(X, tf.transpose(X2))
+
+    def Kdiag(self, X):
+        return self.variance * tf.reshape(X, (-1,))
+
 
 class IterativeLearner(ABC):
     '''
@@ -19,18 +36,18 @@ class IterativeLearner(ABC):
     @param kern: covariance function/kernel for the iterative learning. Must be instance of gpflow.kernels.Kernel     
     @param init_training: size of the initial training set (10 is default)
     '''
-    def __init__(self, 
-                 xs: np.ndarray, 
-                 ys: np.ndarray, 
-                 kern: gpflow.kernels.Kernel = gpflow.kernels.RBF(input_dim=1),
-                 init_training: int = 3):
+
+    def __init__(self,
+                 xs: np.ndarray,
+                 ys: np.ndarray,
+                 kern: gpflow.kernels.Kernel=gpflow.kernels.RBF(input_dim=1),
+                 init_training: int=3):
         self.kernel = kern
         self.scaler = preprocessing.StandardScaler().fit(ys.reshape(-1, 1))
         self.xs = xs.reshape(-1, 1)
         self.ys = self.scaler.transform(ys.reshape(-1, 1))
         self.training_set = np.random.choice(self.xs.reshape(1, -1)[0], size=init_training)
         self.training_set = np.append(self.training_set, [0, len(self.training_set)])
-        
     
     def _transform(self, ys: np.ndarray) -> np.ndarray:
         '''
@@ -57,13 +74,12 @@ class IterativeLearner(ABC):
         xs = self.xs[self.training_set] * 1.0
         self.model = gpflow.models.GPR(
                 xs, self.ys[self.training_set],
-                kern = self.kernel
+                kern=self.kernel
         )
-        self.model.likelihood.variance=0.01
+        self.model.likelihood.variance = 0.01
         gpflow.train.ScipyOptimizer().minimize(self.model)
-
     
-    def iterative_train(self, max_iter: int = 200) -> Tuple[np.array, np.array]:
+    def iterative_train(self, max_iter: int=200) -> Tuple[np.array, np.array]:
         '''
         Train, acquire next, and repeat. This is the main training method for the IterativeLearner.
         
@@ -104,6 +120,7 @@ class IterativeLearner(ABC):
     
     def _status(self):
         print("Training set: {} elements".format(len(set(self.training_set))))
+
        
 class IterativeRandomLearner(IterativeLearner):
     '''
@@ -111,11 +128,12 @@ class IterativeRandomLearner(IterativeLearner):
     the acquire_next method accordingly. The acquisition function selects an arbitrary 
     data point and adds this to the training set.
     '''
-    def __init__(self, 
-                 xs: np.ndarray, 
-                 ys: np.ndarray, 
-                 kernel: gpflow.kernels.Kernel = gpflow.kernels.RBF(input_dim=1),
-                 init_training: int = 3):
+
+    def __init__(self,
+                 xs: np.ndarray,
+                 ys: np.ndarray,
+                 kernel: gpflow.kernels.Kernel=gpflow.kernels.RBF(input_dim=1),
+                 init_training: int=3):
         IterativeLearner.__init__(self, xs, ys, kernel, init_training)
                   
     def acquire_next(self) -> Tuple[np.array, np.array]:
@@ -131,6 +149,7 @@ class IterativeRandomLearner(IterativeLearner):
         self.training_set = np.append(self.training_set, nxt)
         
         return means, variance
+
         
 class ActiveLearner(IterativeLearner):
     '''
@@ -138,11 +157,12 @@ class ActiveLearner(IterativeLearner):
     implementing the acquire_next method accordingly. The acquisition function selects the 
     data point with the largest uncertainty/smallest confidence and adds this to the training set.
     '''
-    def __init__(self, 
-                 xs: np.ndarray, 
-                 ys: np.ndarray, 
-                 kernel: gpflow.kernels.Kernel = gpflow.kernels.RBF(input_dim=1),
-                 init_training: int = 3):
+
+    def __init__(self,
+                 xs: np.ndarray,
+                 ys: np.ndarray,
+                 kernel: gpflow.kernels.Kernel=gpflow.kernels.RBF(input_dim=1),
+                 init_training: int=3):
         IterativeLearner.__init__(self, xs, ys, kernel, init_training)
                   
     def acquire_next(self) -> Tuple[np.array, np.array]:
@@ -157,6 +177,7 @@ class ActiveLearner(IterativeLearner):
         self.training_set = np.append(self.training_set, nxt)
         
         return means, std
+
    
 class BalancedActiveLearner(ActiveLearner):
     '''
@@ -164,14 +185,15 @@ class BalancedActiveLearner(ActiveLearner):
     but excludes data points from the training set if the training set size exceeds a balance limit. This 
     is to limit the training effort and extract data points contributing best to the GP estimate.
     '''
-    def __init__(self, 
-                 xs: np.ndarray, 
-                 ys: np.ndarray, 
-                 kernel: gpflow.kernels.Kernel = gpflow.kernels.RBF(input_dim=1),
-                 init_training: int = 3,
-                 balance_limit = 200):
+
+    def __init__(self,
+                 xs: np.ndarray,
+                 ys: np.ndarray,
+                 kernel: gpflow.kernels.Kernel=gpflow.kernels.RBF(input_dim=1),
+                 init_training: int=3,
+                 balance_limit=200):
         ActiveLearner.__init__(self, xs=xs, ys=ys, kernel=kernel, init_training=init_training)
-        self.balance_limit=balance_limit
+        self.balance_limit = balance_limit
         
     def acquire_next(self) -> Tuple[np.array, np.array]:
         '''
@@ -197,6 +219,4 @@ class BalancedActiveLearner(ActiveLearner):
         self.training_set = np.append(self.training_set, nxt)
         
         return means, std
-
-            
         
