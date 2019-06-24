c894c5217e5b60e8cdd65fc615415ba2988cb93e
Stefan <stefan.muehlbauer@uni-weimar.de>
2019-06-24 13:17:59 +0200

    nice

--- synthetic.py
+++ synthetic.py
@@ -21,62 +21,62 @@ def create_signal():
 
 if __name__ == "__main__":
     u = uuid.uuid4()    
-    for i in range(10):
-        signal, cps = create_signal()
-        prefix = sys.argv[1]
-        if not prefix.endswith("/"):
-            prefix = prefix + "/"
-        
-        MAXITER = 50
-        
-        a = ActiveLearner(np.arange(0, 1000), signal, Brownian())
-        means, variance = a.iterative_train(max_iter=MAXITER)
-        np.savez_compressed(
-            "{}{}_{}_Brownian".format(prefix, i, u),
-            means=means,
-            variance=variance,
-            signal=signal,
-            cps=cps
-        )
-        
-        b = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.Matern32(input_dim=1))
-        means, variance = b.iterative_train(max_iter=MAXITER)
-        np.savez_compressed(
-            "{}{}_{}_Matern32".format(prefix, i, u),
-            means=means,
-            variance=variance,
-            signal=signal,
-            cps=cps
-        )
-        
-        c = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.Matern52(input_dim=1))
-        means, variance = c.iterative_train(max_iter=MAXITER)
-        np.savez_compressed(
-            "{}{}_{}_Matern52".format(prefix, i, u),
-            means=means,
-            variance=variance,
-            signal=signal,
-            cps=cps
-        )
-        
-        d = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.RBF(input_dim=1))
-        means, variance = d.iterative_train(max_iter=MAXITER)
-        np.savez_compressed(
-            "{}{}_{}_RBF".format(prefix, i, u),
-            means=means,
-            variance=variance,
-            signal=signal,
-            cps=cps
-        )
-        
-        e = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.RationalQuadratic(input_dim=1))
-        means, variance = e.iterative_train(max_iter=MAXITER)
-        np.savez_compressed(
-            "{}{}_{}_RationalQuadratic".format(prefix, i, u),
-            means=means,
-            variance=variance,
-            signal=signal,
-            cps=cps
-        )
+    i = 42
+    signal, cps = create_signal()
+    prefix = sys.argv[1]
+    if not prefix.endswith("/"):
+        prefix = prefix + "/"
+    
+    MAXITER = 50
+    
+    a = ActiveLearner(np.arange(0, 1000), signal, Brownian())
+    means, variance = a.iterative_train(max_iter=MAXITER)
+    np.savez_compressed(
+        "{}{}_{}_Brownian".format(prefix, i, u),
+        means=means,
+        variance=variance,
+        signal=signal,
+        cps=cps
+    )
+    
+    b = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.Matern32(input_dim=1))
+    means, variance = b.iterative_train(max_iter=MAXITER)
+    np.savez_compressed(
+        "{}{}_{}_Matern32".format(prefix, i, u),
+        means=means,
+        variance=variance,
+        signal=signal,
+        cps=cps
+    )
+    
+    c = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.Matern52(input_dim=1))
+    means, variance = c.iterative_train(max_iter=MAXITER)
+    np.savez_compressed(
+        "{}{}_{}_Matern52".format(prefix, i, u),
+        means=means,
+        variance=variance,
+        signal=signal,
+        cps=cps
+    )
+    
+    d = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.RBF(input_dim=1))
+    means, variance = d.iterative_train(max_iter=MAXITER)
+    np.savez_compressed(
+        "{}{}_{}_RBF".format(prefix, i, u),
+        means=means,
+        variance=variance,
+        signal=signal,
+        cps=cps
+    )
+    
+    e = ActiveLearner(np.arange(0, 1000), signal, gpflow.kernels.RationalQuadratic(input_dim=1))
+    means, variance = e.iterative_train(max_iter=MAXITER)
+    np.savez_compressed(
+        "{}{}_{}_RationalQuadratic".format(prefix, i, u),
+        means=means,
+        variance=variance,
+        signal=signal,
+        cps=cps
+    )
     
     
