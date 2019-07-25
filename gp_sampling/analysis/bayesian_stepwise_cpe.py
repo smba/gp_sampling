aa756404cdcc016f0c825ed419c97c8db0cd747b
Stefan Mühlbauer <stefan.muehlbauer@uni-weimar>
2019-07-25 09:48:11 +0200

    beneer

--- gp_sampling/analysis/bayesian_stepwise_cpe.py
+++ gp_sampling/analysis/bayesian_stepwise_cpe.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import pandas as pd
 import numpy as np
 import pymc3 as pm
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sklearn.mixture import BayesianGaussianMixture
 
 sns.set_context("paper")
 plt.style.use('bmh')
@@ -17,54 +19,65 @@ class BayesianStepwiseCPE:
     def __init__(self, signal: np.ndarray):
         self.signal = signal
         
-    def estimate_states(self, n_components = 10):
+    def estimate_states(self, n_components = 15):
+        mix = BayesianGaussianMixture(
+            n_components, 
+            n_init = 25,
+            weight_concentration_prior_type = "dirichlet_process",
+            #warm_start = True,
+            verbose = 1,
+            tol = 1e-12,
+            max_iter = 1000
+        )
+        s = self.signal[~np.isnan(self.signal)].reshape(-1, 1)
+        mix.fit(s)
         
-        K = n_components
-        N = self.signal.shape[0]
+        a = plt.subplot(1,2,1)
         
-        with pm.Model() as model:
-            w = pm.Dirichlet('w', np.ones(K))
-
-            mu = pm.Normal('mu', 0., 10., shape=K)
-            tau = pm.Gamma('tau', 1., 1., shape=K)
+        #sns.distplot(s, ax = a, bins=50, color="darkgrey", norm_hist=True)
         
-            s = self.signal[~np.isnan(self.signal)]
-            x_obs = pm.NormalMixture('x_obs', w, mu, tau=tau, observed=s)
+        means = mix.means_
+        sds = mix.covariances_
+        weights = mix.weights_
+        rand = 0.1
+        xmin = np.min(s) - rand * np.mean(s)
+        xmax = np.max(s) + rand * np.mean(s)
+        xs = np.linspace(xmin, xmax, 500)
+        
+        for i in range(means.shape[0]):
+            sd = sds[i, 0, 0]
+            mu = means[i ,0]
             
-            self.trace = dict()
-            trace = pm.sample(5000, n_init=10000, tune=1000, random_seed=123)
-            retain = 500
-            for rv in ["mu", "tau", "w"]:
-                self.trace[rv] = np.array( trace[rv][- retain:] )
+            if weights[i] > 0.05:
+                pdf = lambda x: norm.pdf(x, loc = mu, scale = sd)
+                ys = list(map(pdf, xs))
+    
+                sns.lineplot(xs, ys, color="firebrick", ax=a, alpha=weights[i]*2)
+        
+        b = plt.subplot(1,2,2)
+        sns.barplot(list(range(n_components)), weights, ax = b, facecolor="steelblue")
+        
+        cps_cnt = 0
+        ss = pd.DataFrame(self.signal)
+        ss = ss.dropna()
+        c = mix.predict(ss)
+        clusters = np.unique(c)
+        cs = c[np.insert(np.diff(c).astype(np.bool), 0, True)]
+        print("identified {} states and {} transitions".format(len(clusters), len(cs) - 1))
                 
-        ax1 = plt.subplot(2,2,1)
-        #sns.distplot(self.signal[~np.isnan(self.signal)], kde=False, ax=ax1, bins=100, norm_hist=True)
-        
-        mean_centroid = self.trace["mu"].mean(0)
-        tau_centroids = self.trace["tau"].mean(0)
-        mean_weights = self.trace["w"].mean(0)
-        sigmas = np.array(list(map(lambda t: 1/t, tau_centroids)))
-        xs = np.linspace(min(self.signal), max(self.signal), 200)
-        
-        posterior = np.zeros(xs.shape[0])
-        for i in range(n_components):
-            print( (mean_centroid[i], mean_weights[i], tau_centroids[i]) )
-            ys = np.array(list(map(lambda x: norm_pdf(x, mean_centroid[i], sigmas[i]), xs)))
-            sns.lineplot(xs, mean_weights[i]* ys, linewidth=0.5, color="mediumblue", linestyle=":", ax=ax1)
-            posterior += mean_weights[i] * ys
-        sns.lineplot(xs, posterior, linewidth=1.2, color="steelblue", ax=ax1, label="GMM")
-        
-        
         plt.show()
+        
                             
 if __name__ == "__main__":
-    a = [np.random.normal(10, 0.4) for i in range(100)] + [np.random.normal(6, 0.5) for i in range(50)] + [np.random.normal(14, 0.3) for i in range(50)]
-    a = np.array(a)
-    sample = np.random.choice(np.arange(a.shape[0]), size = 50)
-    signal = np.full(a.shape, np.nan)
-    signal[sample] = a[sample]
+    signal = pd.read_csv("/home/stefan/git/gp_sampling/resources/ground_truth/pillow.csv")
+    signal = signal[signal.columns[23]][:]
+    signal = pd.DataFrame(signal).values.reshape(1, -1)[0]
+    plain = np.full(signal.shape, np.nan)
+    np.random.seed(42)
+    sample = np.random.choice(np.arange(signal.shape[0]), size=7999)
+    plain[sample] = signal[sample]
     
-    c = BayesianStepwiseCPE(a)
-    c.estimate_states(10)
+    c = BayesianStepwiseCPE(plain)
+    c.estimate_states()
                     
                     
